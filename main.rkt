f1128e5041e97db9d226a545570220974ef357e2
Giorgio Di Rosa <63345060+gdrose@users.noreply.github.com>
2026-04-02 17:27:54 +0200

    feat: window init with raylib

--- main.rkt
+++ main.rkt
@@ -1,3 +1,14 @@
 #lang racket
+(require raylib/2d/unsafe)
 
-"Hello Pizza"
+(InitWindow 960 540 "Pizzario - POPL CS")
+
+  (let loop ()
+    (when (not (WindowShouldClose))
+      (BeginDrawing)
+      (ClearBackground RAYWHITE)
+      (DrawText "I hate racket" 10 10 20 LIME)
+      (EndDrawing)
+      (loop)))
+
+  (CloseWindow)
