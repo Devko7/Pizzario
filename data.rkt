8eb33325702625514ba9fe6fe51b3372e1ebca69
David Vasilev <dvvasilev@proton.me>
2026-04-09 16:38:39 +0200

    toppings and sauces

--- data.rkt
+++ data.rkt
@@ -1,6 +1,16 @@
 #lang racket
 
+;; Toppings
+(define pepperoni (ingredient "topping" "Pepperoni" "pepperoni.png" 5))
+(define mushrooms (ingredient "topping" "Mushrooms" "mushrooms.png" 2))
+(define ham (ingredient "topping" "Ham" "ham.png" 4))
+(define fries (ingredient "topping" "Fries" "fries.png" 7))
+(define turkey (ingredient "topping" "Turkey" "turkey.png" 5))
+(define corn (ingredient "topping" "Corn" "corn.png" 2))
 
+;; Sauces
+(define tomato-sauce (ingredient "sauce" "Tomato Sauce" "tomatoSauce.png" 1))
+(define cream (ingredient "sauce" "Cream" "cream.png" 1))
 
 ;; BELOW GIORGIO - ABOVE DAVID
 
